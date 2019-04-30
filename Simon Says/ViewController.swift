683c8cedb30f810e2246fcf8d2ca1c6c25119cdb
NP2 <home@NP2s-iMac-2.local>
2019-04-30 14:23:43 +0430

    Wrote the very first logic of the game including, button animation and start button action

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -11,6 +11,13 @@ import SHNDStuffs
 
 final class ViewController: UIViewController {
 
+    
+    private var currentPlayer = 0
+    private var scores = [0,0]
+    private var sequenceIndex = 0
+    private var colorSequence = Array<Int>()
+    private var colorsToTap = Array<Int>()
+    
     @IBOutlet var colorButtons: [SHNDCircularButton]!
     @IBOutlet weak var actionButton: UIButton!
     @IBOutlet var playerLabels: [UILabel]!
@@ -22,12 +29,25 @@ final class ViewController: UIViewController {
     
     
     @IBAction func actionButtonHandler(_ sender: UIButton) {
+        sequenceIndex = 0
+        actionButton.setTitle("Memorize", for: .normal)
+        actionButton.isEnabled = false
+        view.isUserInteractionEnabled = false
+        addNewColor()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
+            guard let shndSelf = self else { return }
+            shndSelf.playSequence()
+        }
     }
     
     private func createNewGame() {
+        colorSequence.removeAll()
+        
         actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
         for button in colorButtons {
             button.alpha = 0.7
+            button.isEnabled = false
         }
     }
     
@@ -42,6 +62,33 @@ final class ViewController: UIViewController {
         
         scoreLabels = scoreLabels.sorted() {
             $0.tag < $1.tag
+        }
+    }
+    
+    private func addNewColor() {
+        colorSequence.append(Int(arc4random_uniform(UInt32(4)))) // random number between 0 ti
+    }
+    
+    private func playSequence() {
+        if sequenceIndex < colorSequence.count {
+            flash(button: colorButtons[sequenceIndex])
+            sequenceIndex += 1
+        } else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap the Circles", for: .normal)
+            for button in colorButtons {
+                button.isEnabled = true
+            }
+        }
+    }
+    
+    private func flash(button:SHNDButton) {
+        UIView.animate(withDuration: 0.5, animations: {
+            button.alpha = 1.0
+            button.alpha = 0.7
+        }) { [unowned self] (success) in
+            self.playSequence()
         }
     }
 }
